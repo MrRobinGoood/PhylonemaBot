280c8b8ac5330338905e152f36a258577b15eaa2
Zlyden-1 <Zlyden-1@yandex.ru>
2023-01-18 00:37:04 +0400

    Added correct rating setting, getting and storage

--- Cinema/CinemaCard.py
+++ Cinema/CinemaCard.py
@@ -2,8 +2,17 @@ import pandas as pd
 
 
 class CinemaCard(object):
-    __path = r'C:/Users/Zlyde/PycharmProjects/PhylonemaBot/resources/Cinema/CinemaCards.csv'
-    cinema_cards_base = pd.read_csv(__path)
+    __path_to_cards = r'C:/Users/Zlyde/PycharmProjects/PhylonemaBot/resources/Cinema/CinemaCards.csv'
+    __path_to_rates = r'C:/Users/Zlyde/PycharmProjects/PhylonemaBot/resources/Cinema/Rates.csv'
+    cinema_cards_base = pd.read_csv(__path_to_cards)
+    rates_base = pd.read_csv(__path_to_rates)
+
+    def calculate_average_rating(self, category: str) -> float:
+        all_rates: pd.DataFrame = self.rates_base[category].loc[(self.rates_base['Название'] == self.name) &
+                                                                (self.rates_base['Автор'] == self.author)]
+        sum_of_rates = sum(all_rates)
+        amount_of_rates = all_rates.count()
+        return sum_of_rates/amount_of_rates
 
     @classmethod
     def add_card_to_csv(cls, name: str = '', author: str = '', timecodes: str = '', link: str = ''):
@@ -21,7 +30,7 @@ class CinemaCard(object):
         for i in rating.keys():
             row.append(rating[i])
         cls.cinema_cards_base.loc[length] = row
-        cls.cinema_cards_base.to_csv(cls.__path, index=False)
+        cls.cinema_cards_base.to_csv(cls.__path_to_cards, index=False)
         return
 
     def __init__(self, name: str = '', author: str = '', timecodes: str = '', link: str = '', ):
@@ -29,24 +38,24 @@ class CinemaCard(object):
         self.author = author
         self.timecodes = timecodes
         self.link = link
-        self.rating: dict = {'Философская глубина': 0,
-                             'Острота постановки проблемы': 0,
-                             'Наличие категориального аппарата': 0,
-                             'Эстетическое удовольствие': 0,
-                             'Насколько берет за душу': 0,
-                             'Раскрытие мировоззрения автора': 0,
-                             'Художественная глубина': 0,
-                             'Общее впечатление': 0}
+        categories = ('Философская глубина',
+                      'Острота постановки проблемы',
+                      'Наличие категориального аппарата',
+                      'Эстетическое удовольствие',
+                      'Насколько берет за душу',
+                      'Раскрытие мировоззрения автора',
+                      'Художественная глубина',
+                      'Общее впечатление')
+        self.rating: dict = {x: self.calculate_average_rating(x) for x in categories}
         self.reviews_amount: int = 0
 
-    def set_rating(self, rating=None):
+    def add_rating(self, rating=None):
         if rating is None:
             rating = [0, 0, 0, 0, 0, 0, 0, 0]
-        if len(rating):
-            k = 0
-            for i in self.rating.keys():
-                self.rating[i] = rating[k]
-                k += 1
+        if len(rating) == 8:
+            length = len(self.rates_base)
+            self.rates_base.loc[length] = rating
+            self.rates_base.to_csv(self.__path_to_rates)
         else:
             raise ValueError('Not enough values for setting rating')
 
@@ -57,7 +66,7 @@ class CinemaCard(object):
         values = film_info.values[0][2:]
         timecodes, link, reviews_amount, *rating = values
         search_result = CinemaCard(name, author, timecodes, link)
-        search_result.set_rating(rating)
+        search_result.add_rating(rating)
         return search_result
 
     def __str__(self):
