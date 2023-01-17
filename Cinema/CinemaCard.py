7a4b6a1c52b848a6afefa1a6acd9b36d7c3d42ad
Zlyden-1 <Zlyden-1@yandex.ru>
2023-01-17 11:40:56 +0400

    Set up correct csv updates

--- Cinema/CinemaCard.py
+++ Cinema/CinemaCard.py
@@ -2,18 +2,18 @@ import pandas as pd
 
 
 class CinemaCard(object):
-    cinema_cards_base = pd.read_csv('C:/Users/Zlyde/PycharmProjects/PhylonemaBot/resources/Cinema')
+    cinema_cards_base = pd.read_csv(r'C:/Users/Zlyde/PycharmProjects/PhylonemaBot/resources/Cinema/CinemaCards.csv')
 
-    async def add_card_to_csv(self):
+    def add_card_to_csv(self):
         length = len(self.cinema_cards_base)
         row = [self.name, self.author, self.timecodes, self.link, self.reviews_amount]
         for i in self.rating.keys():
             row.append(self.rating[i])
         self.cinema_cards_base.loc[length] = row
+        self.cinema_cards_base.to_csv(r'C:/Users/Zlyde/PycharmProjects/PhylonemaBot/resources/Cinema/CinemaCards.csv',
+                                      index=False)
 
-    def __init__(self, name: str = '', author: str = '', timecodes=None, link: str = ''):
-        if timecodes is None:
-            timecodes = {}
+    def __init__(self, name: str = '', author: str = '', timecodes: str = '', link: str = ''):
         self.name = name
         self.author = author
         self.timecodes = timecodes
@@ -27,8 +27,5 @@ class CinemaCard(object):
                              'Художественная глубина': 0,
                              'Общее впечатление': 0}
         self.reviews_amount: int = 0
-        await self.add_card_to_csv()
-
-
-
+        self.add_card_to_csv()
 
